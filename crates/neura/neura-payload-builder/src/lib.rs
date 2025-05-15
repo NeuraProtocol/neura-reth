0ce36d7210421cbf237de3b284726535e43c92a3
PeaStew <peter.gavin.stewart@gmail.com>
2025-05-15 00:24:51 +0000

    still on a road to somewhere.not working

--- crates/neura/neura-payload-builder/src/lib.rs
+++ crates/neura/neura-payload-builder/src/lib.rs
@@ -1,33 +1,21 @@
+//! Neura-specific payload builder.
+
 use neura_payload_types::{NeuraBuiltPayloadWithDetails, FullTransaction, TransactionConverter};
 use reth_ethereum_payload_builder::EthereumPayloadBuilder as RethEthereumPayloadBuilder;
 use reth_basic_payload_builder::PayloadBuilder;
 use reth_ethereum_engine_primitives::EthPayloadBuilderAttributes;
 use reth_payload_builder_primitives::PayloadBuilderError;
-use reth_basic_payload_builder::{BuildArguments, BuildOutcome, MissingPayloadBehaviour, PayloadConfig};
-use reth_ethereum_engine_primitives::EthBuiltPayload;
-use reth_ethereum_primitives::{TransactionSigned, EthPrimitives};
-use reth_transaction_pool::{TransactionPool, PoolTransaction};
+use reth_basic_payload_builder::{MissingPayloadBehaviour};
+use reth_ethereum_engine_primitives::EthBuiltPayload; // For EthBuiltPayload type
+use reth_ethereum_primitives::EthPrimitives; // For EvmConfig generic constraint & block header
+use reth_transaction_pool::TransactionPool;
 use reth_storage_api::StateProviderFactory;
 use reth_chainspec::{ChainSpecProvider, EthereumHardforks};
 use reth_evm::ConfigureEvm;
 use reth_evm_ethereum::EthEvmConfig;
-use reth_primitives_traits::{Header as RethHeaderTrait, NodePrimitives, Block as RethBlockTrait, SealedBlock as RethPrimitivesSealedBlock};
-use alloy_rpc_types::eth::{Transaction as AlloyTransaction, AccessList as AlloyRpcAccessList, AccessListItem as AlloyRpcAccessListItem};
-use alloy_primitives::{U256, Address, Bytes, B256, U64, TxKind as AlloyCoreTxKind, Signature as AlloyCoreSignature};
-use alloy_eips::eip7685::Requests;
-use alloy_consensus::{
-    Signed, TxLegacy, TxEip2930, TxEip1559, TxEip4844, TxEip7702, 
-    TxType as AlloyCoreTxType, 
-    EthereumTxEnvelope, TxEip4844Variant,
-    BlobTransactionSidecar as AlloyBlobTransactionSidecar,
-    SignableTransaction, 
-};
-use alloy_consensus::transaction::Recovered;
-use alloy_rlp::Encodable;
-use alloy_eips::eip2930::{AccessList as AlloyEip2930AccessList, AccessListItem as AlloyEip2930AccessListItem};
-use alloy_primitives::eip4844::{Blob, KzgCommitment, KzgProof};
+
 use std::sync::Arc;
-use reth_ethereum_primitives::{Transaction as RethTransactionEnum};
+
 
 /// A Neura-specific payload builder that wraps the standard Reth EthereumPayloadBuilder
 /// to augment its output with `FullTransaction` details.
@@ -40,9 +28,10 @@ impl<Pool, Client, EvmConfig> NeuraPayloadBuilder<Pool, Client, EvmConfig>
 where
     Client: StateProviderFactory + ChainSpecProvider + Clone + Send + Sync + 'static,
     <Client as ChainSpecProvider>::ChainSpec: EthereumHardforks,
-    Pool: TransactionPool<Transaction = Arc<reth_transaction_pool::ValidPoolTransaction<TransactionSigned>>, Consensus = TransactionSigned> + Clone + Send + Sync + 'static,
+    Pool: TransactionPool<Transaction = reth_transaction_pool::EthPooledTransaction> + Clone + Send + Sync + 'static,
     EvmConfig: ConfigureEvm<Primitives = EthPrimitives, NextBlockEnvCtx = reth_evm::NextBlockEnvAttributes> + Clone + Send + Sync + 'static,
 {
+    /// Creates a new instance of `NeuraPayloadBuilder`.
     pub fn new(
         client: Client,
         pool: Pool,
@@ -54,97 +43,24 @@ where
         }
     }
 
-    fn convert_to_full_transactions(
-        &self,
+    fn convert_to_full_transactions_static(
         built_payload: &EthBuiltPayload,
     ) -> Vec<FullTransaction> {
         let sealed_block_concrete = built_payload.block();
-        let block_header_ref: &alloy_consensus::Header = sealed_block_concrete.header();
-        let envelopes_from_block: &[EthereumTxEnvelope<TransactionSigned>] = sealed_block_concrete.body(); 
+        // let block_header_ref = sealed_block_concrete.header(); // Not used if effective_gas_price is handled in converter
         
-        let base_fee_per_gas: Option<u64> = block_header_ref.base_fee_per_gas();
+        let envelopes_from_block: &[alloy_consensus::EthereumTxEnvelope<alloy_consensus::TxEip4844>] = 
+            &sealed_block_concrete.body().transactions; 
+        
+        // base_fee_per_gas is not needed here if the converter handles it or if FullTransaction doesn't store effective_gas_price
+        // let base_fee_per_gas: Option<u64> = block_header_ref.base_fee_per_gas;
 
         envelopes_from_block
-            .iter() 
-            .enumerate()
-            .filter_map(|(_idx, envelope)| { 
-                let reth_tx_signed: TransactionSigned = match envelope {
-                    EthereumTxEnvelope::Legacy(s) => TransactionSigned::from(s.clone()),
-                    EthereumTxEnvelope::Eip2930(s) => TransactionSigned::from(s.clone()),
-                    EthereumTxEnvelope::Eip1559(s) => TransactionSigned::from(s.clone()),
-                    EthereumTxEnvelope::Eip4844(variant) => match variant {
-                        TxEip4844Variant::Tx(s) => TransactionSigned::from(s.clone()),
-                        _ => return None,
-                    },
-                    EthereumTxEnvelope::Eip7702(s) => TransactionSigned::from(s.clone()),
-                    _ => return None,
-                };
-
-                let alloy_signature: AlloyCoreSignature = reth_tx_signed.signature().clone();
-                let reth_tx_variant = &reth_tx_signed.transaction; 
-                let tx_hash = reth_tx_signed.hash();
-
-                let convert_reth_access_list_to_consensus =
-                    |reth_al_opt: Option<&AlloyEip2930AccessList>| -> Option<AlloyEip2930AccessList> {
-                        reth_al_opt.cloned()
-                    };
-                
-                let convert_reth_tx_kind_to_consensus =
-                    |reth_kind: &AlloyCoreTxKind| -> AlloyCoreTxKind {
-                        *reth_kind
-                    };
-
-                let inner_tx_envelope = match reth_tx_variant {
-                    RethTransactionEnum::Legacy(unsigned_tx) => {
-                        let signed_tx = Signed::new_unchecked(unsigned_tx.clone(), alloy_signature, *tx_hash);
-                        EthereumTxEnvelope::Legacy(signed_tx)
-                    }
-                    RethTransactionEnum::Eip2930(unsigned_tx) => {
-                        let signed_tx = Signed::new_unchecked(unsigned_tx.clone(), alloy_signature, *tx_hash);
-                        EthereumTxEnvelope::Eip2930(signed_tx)
-                    }
-                    RethTransactionEnum::Eip1559(unsigned_tx) => {
-                        let signed_tx = Signed::new_unchecked(unsigned_tx.clone(), alloy_signature, *tx_hash);
-                        EthereumTxEnvelope::Eip1559(signed_tx)
-                    }
-                    RethTransactionEnum::Eip4844(unsigned_tx) => {
-                        let signed_tx = Signed::new_unchecked(unsigned_tx.clone(), alloy_signature, *tx_hash);
-                        EthereumTxEnvelope::Eip4844(TxEip4844Variant::Tx(signed_tx))
-                    }
-                    RethTransactionEnum::Eip7702(unsigned_tx) => {
-                        let signed_tx = Signed::new_unchecked(unsigned_tx.clone(), alloy_signature, *tx_hash);
-                        EthereumTxEnvelope::Eip7702(signed_tx)
-                    }
-                    _ => return None,
-                };
-                
-                let inner_recovered_tx = Recovered::new_unchecked(inner_tx_envelope.clone(), reth_tx_signed.signer());
-
-                let calculated_effective_gas_price = {
-                    let base_fee = base_fee_per_gas.map(U256::from).unwrap_or_default();
-                    U256::from(reth_tx_variant.effective_gas_price(base_fee_per_gas))
-                };
-                
-                let rpc_access_list = reth_tx_variant.access_list().map(|al_ref| {
-                    AlloyRpcAccessList(
-                        al_ref.0.iter().map(|item: &AlloyEip2930AccessListItem| {
-                            AlloyRpcAccessListItem {
-                                address: item.address,
-                                storage_keys: item.storage_keys.clone(),
-                            }
-                        }).collect::<Vec<_>>()
-                    )
-                });
-
-                let alloy_tx = AlloyTransaction {
-                    inner: inner_recovered_tx,
-                    effective_gas_price: Some(calculated_effective_gas_price.to::<u128>()),
-                    block_hash: None,
-                    block_number: None,
-                    transaction_index: None,
-                };
-
-                Some(alloy_tx.into_full_type())
+            .iter()
+            .filter_map(|envelope| { // envelope is &alloy_consensus::EthereumTxEnvelope<TxEip4844>
+                // Directly convert the envelope, assuming TransactionConverter is impl'd for it.
+                // The envelope needs to be cloned because into_full_type takes self.
+                Some(TransactionConverter::into_full_type(envelope.clone()))
             })
             .collect()
     }
@@ -155,7 +71,7 @@ where
     EvmConfig: ConfigureEvm<Primitives = EthPrimitives, NextBlockEnvCtx = reth_evm::NextBlockEnvAttributes> + Clone + Send + Sync + 'static,
     Client: StateProviderFactory + ChainSpecProvider + Clone + Unpin + Send + Sync + 'static,
     <Client as ChainSpecProvider>::ChainSpec: EthereumHardforks,
-    Pool: TransactionPool<Transaction = Arc<reth_transaction_pool::ValidPoolTransaction<TransactionSigned>>, Consensus = TransactionSigned> + Clone + Send + Sync + 'static,
+    Pool: TransactionPool<Transaction = reth_transaction_pool::EthPooledTransaction> + Clone + Send + Sync + 'static,
 {
     type Attributes = EthPayloadBuilderAttributes;
     type BuiltPayload = NeuraBuiltPayloadWithDetails;
@@ -173,13 +89,13 @@ where
             cached_reads: args.cached_reads,
             config: args.config, 
             cancel: args.cancel, 
-            best_payload: original_best_payload_opt,
+            best_payload: original_best_payload_opt.map(|arc_payload| (*arc_payload).clone()),
         };
 
         match self.inner_builder.try_build(inner_args) {
             Ok(reth_basic_payload_builder::BuildOutcome::Better { payload: eth_payload, cached_reads }) => {
-                let full_transactions = self.convert_to_full_transactions(&eth_payload);
-                let neura_payload = NeuraBuiltPayloadWithDetails::new((*eth_payload).clone(), full_transactions);
+                let full_transactions = Self::convert_to_full_transactions_static(&eth_payload);
+                let neura_payload = NeuraBuiltPayloadWithDetails::new(eth_payload.clone(), full_transactions);
                 Ok(reth_basic_payload_builder::BuildOutcome::Better { payload: neura_payload, cached_reads })
             }
             Ok(reth_basic_payload_builder::BuildOutcome::Aborted { fees, cached_reads }) => {
@@ -187,8 +103,8 @@ where
             }
             Ok(reth_basic_payload_builder::BuildOutcome::Cancelled) => Ok(reth_basic_payload_builder::BuildOutcome::Cancelled),
             Ok(reth_basic_payload_builder::BuildOutcome::Freeze(eth_payload)) => {
-                let full_transactions = self.convert_to_full_transactions(&eth_payload);
-                let neura_payload = NeuraBuiltPayloadWithDetails::new((*eth_payload).clone(), full_transactions);
+                let full_transactions = Self::convert_to_full_transactions_static(&eth_payload);
+                let neura_payload = NeuraBuiltPayloadWithDetails::new(eth_payload.clone(), full_transactions);
                 Ok(reth_basic_payload_builder::BuildOutcome::Freeze(neura_payload))
             }
             Err(e) => Err(e),
@@ -207,7 +123,7 @@ where
             cached_reads: args.cached_reads,
             config: args.config.clone(),
             cancel: args.cancel.clone(),
-            best_payload: original_best_payload_opt,
+            best_payload: original_best_payload_opt.map(|arc_payload| (*arc_payload).clone()),
         };
 
         match self.inner_builder.on_missing_payload(inner_args) {
@@ -216,8 +132,8 @@ where
             MissingPayloadBehaviour::RacePayload(job_fn) => {
                 MissingPayloadBehaviour::RacePayload(Box::new(move || {
                     job_fn().map(|eth_payload_arc| {
-                        let full_transactions = self.convert_to_full_transactions(&eth_payload_arc);
-                        NeuraBuiltPayloadWithDetails::new((*eth_payload_arc).clone(), full_transactions)
+                        let full_transactions = Self::convert_to_full_transactions_static(&eth_payload_arc);
+                        NeuraBuiltPayloadWithDetails::new(eth_payload_arc.clone(), full_transactions)
                     })
                 }))
             }
@@ -230,10 +146,10 @@ where
     ) -> Result<Self::BuiltPayload, PayloadBuilderError> {
         match self.inner_builder.build_empty_payload(config) {
             Ok(eth_payload_arc) => {
-                let full_transactions = self.convert_to_full_transactions(&eth_payload_arc);
-                Ok(NeuraBuiltPayloadWithDetails::new((*eth_payload_arc).clone(), full_transactions))
+                let full_transactions = Self::convert_to_full_transactions_static(&eth_payload_arc);
+                Ok(NeuraBuiltPayloadWithDetails::new(eth_payload_arc.clone(), full_transactions))
             }
             Err(e) => Err(e),
         }
     }
-} 
+}
--- crates/neura/neura-payload-types/src/lib.rs
+++ crates/neura/neura-payload-types/src/lib.rs
@@ -1,13 +1,14 @@
-use alloy_primitives::{Address, Bytes, U256, B256};
-use alloy_rpc_types::eth::{Transaction as AlloyTransaction, AccessList as AlloyAccessList};
+//! Crate for Neura-specific payload types.
+
+use alloy_primitives::{Address, Bytes, U256, TxKind};
+use alloy_rpc_types::eth::{Transaction as AlloyTransaction};
 use alloy_rpc_types::TransactionTrait; // Import the trait
+use alloy_consensus::{TxType}; // TxKind is NOT imported from here
 use reth_ethereum_engine_primitives::EthBuiltPayload;
 use reth_payload_primitives::BuiltPayload;
 use reth_primitives_traits::{SealedBlock, NodePrimitives};
 use reth_ethereum_primitives::EthPrimitives; // For BuiltPayload trait
 use alloy_eips::eip7685::Requests; // For BuiltPayload trait
-use alloy_consensus::transaction::EthereumTxEnvelope;
-use alloy_consensus::TxEip4844Variant;
 
 // Potentially enable serde if the feature is active
 #[cfg(feature = "serde")]
@@ -17,59 +18,169 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct FullTransaction {
+    /// The recipient address of the transaction.
     pub to: Option<Address>,
+    /// The value (amount) transferred in the transaction.
     pub value: U256,
+    /// The input data (calldata) of the transaction.
     pub input: Bytes,
+    /// The nonce of the sender.
     pub nonce: u64,
+    /// The gas limit (maximum gas) for the transaction.
     pub gas_limit: u64,
+    /// The gas price for legacy transactions.
     pub gas_price: Option<U256>,
+    /// The chain ID for replay protection.
     pub chain_id: Option<u64>,
+    /// The R value of the transaction signature.
     pub r: U256,
+    /// The S value of the transaction signature.
     pub s: U256,
+    /// The V value of the transaction signature (EIP-155 adjusted).
     pub v: U256,
-    pub y_parity: Option<U256>, // Represents Parity (0 for even, 1 for odd)
+    /// The Y-parity of the signature (0 for even, 1 for odd).
+    pub y_parity: Option<U256>,
+    /// The access list for EIP-2930 transactions.
     pub access_list: Option<Vec<(Address, Vec<U256>)>>,
+    /// The type of the transaction (e.g., legacy, EIP-1559).
     pub transaction_type: Option<U256>,
+    /// The maximum fee per gas for EIP-1559 transactions.
     pub max_fee_per_gas: Option<U256>,
+    /// The maximum priority fee per gas for EIP-1559 transactions.
     pub max_priority_fee_per_gas: Option<U256>,
 }
 
 /// A trait for converting different transaction-related types to their "full" counterparts.
 pub trait TransactionConverter {
+    /// The associated full type representation.
     type FullType;
+    /// Converts the transaction type into its "full" representation.
     fn into_full_type(self) -> Self::FullType;
 }
 
 impl TransactionConverter for AlloyTransaction {
     type FullType = FullTransaction;
 
+    /// Converts an `alloy_rpc_types::eth::Transaction` into a `FullTransaction`.
     fn into_full_type(self) -> Self::FullType {
-        let envelope: EthereumTxEnvelope<TxEip4844Variant> = self.inner.clone_inner(); // Assuming this gives EthereumTxEnvelope
-        let signature = envelope.signature(); // Assuming this gives alloy_primitives::Signature
+        // `AlloyTransaction` (self) wraps an `inner: Recovered<EthereumTxEnvelope<Signature>>`.
+        // `TransactionTrait` methods are implemented on `EthereumTxEnvelope` found at `self.inner.message`.
+        // RPC-specific, processed fields (r, s, v, y_parity) are direct fields on `self`.
+
+        let access_list_converted = self.inner.access_list().map(|al_ref| {
+            al_ref.0.iter()
+                .map(|item| {
+                    let keys_u256 = item
+                        .storage_keys
+                        .iter()
+                        .map(|k| U256::from_be_bytes(k.0))
+                        .collect();
+                    (item.address, keys_u256)
+                })
+                .collect()
+        });
+
+        // Use .kind() method from TransactionTrait on self.inner.message
+        // TxKind is from alloy_primitives
+        let to_address = if let TxKind::Call(addr) = self.inner.kind() { Some(addr) } else { None };
+
+        // Use .tx_type() and .max_fee_per_gas() from TransactionTrait on self.inner.message
+        let max_fee_val = match self.inner.tx_type() {
+            TxType::Eip1559 | TxType::Eip4844 | TxType::Eip7702 => Some(U256::from(self.inner.max_fee_per_gas())),
+            _ => None,
+        };
 
         FullTransaction {
-            to: envelope.to(),
-            value: envelope.value(),
-            input: envelope.input().clone(),
-            nonce: envelope.nonce(),
-            gas_limit: envelope.gas_limit(),
-            gas_price: Some(U256::from(envelope.gas_price())),
-            chain_id: envelope.chain_id(),
+            to: to_address,
+            value: self.inner.value(),
+            input: self.inner.input().clone(),
+            nonce: self.inner.nonce(),
+            gas_limit: self.inner.gas_limit(),
+            gas_price: self.inner.gas_price().map(U256::from),
+            chain_id: self.inner.chain_id(),
+            
+            // Access signature components from self.inner.signature using methods
+            r: self.inner.signature().r(), 
+            s: self.inner.signature().s(), 
+            // v: self.inner.signature.v(), // Previous attempt for v from sig.rs, which returns bool (y_parity)
+            // For FullTransaction.v, we expect the EIP-155 adjusted V. Let's retry self.v from AlloyTransaction itself.
+            // If alloy_rpc_types::Transaction v0.7.0 does not have `v` field, this will error again, and we'll need another source for it.
+            // v: self.v, // This errored: no field `v` on type `alloy_rpc_types::Transaction`
+            
+            // Reconstruct EIP-155 `v` value
+            v: {
+                let y_parity_val = self.inner.signature().v() as u64;
+                if let Some(chain_id) = self.inner.chain_id() {
+                    U256::from(y_parity_val + 35 + chain_id * 2)
+                } else {
+                    // Legacy transaction (no chain_id)
+                    U256::from(y_parity_val + 27)
+                }
+            },
+            
+            // y_parity is derived from signature's v() method (which returns a bool for y_parity)
+            y_parity: Some(if self.inner.signature().v() { U256::from(1) } else { U256::from(0) }),
+            
+            access_list: access_list_converted, 
+            
+            transaction_type: Some(U256::from(self.inner.tx_type() as u8)),
+            
+            max_fee_per_gas: max_fee_val,
+            max_priority_fee_per_gas: self.inner.max_priority_fee_per_gas().map(U256::from),
+        }
+    }
+}
+
+impl TransactionConverter for alloy_consensus::EthereumTxEnvelope<alloy_consensus::TxEip4844> {
+    type FullType = FullTransaction;
+
+    fn into_full_type(self) -> Self::FullType { // self is now EthereumTxEnvelope<TxEip4844>
+        let _signer = self.recover_signer().unwrap_or_default(); // EthereumTxEnvelope directly has this if TxEnvelope trait is used
+        let signature = self.signature(); // EthereumTxEnvelope directly has this if TxEnvelope trait is used
+
+        // Access other fields using methods from TransactionTrait, which EthereumTxEnvelope implements
+        let to_address = if let TxKind::Call(addr) = self.kind() { Some(addr) } else { None };
+
+        let max_fee_val = match self.tx_type() {
+            TxType::Eip1559 | TxType::Eip4844 | TxType::Eip7702 => Some(U256::from(self.max_fee_per_gas())),
+            _ => None,
+        };
+        
+        // Note: effective_gas_price might need base_fee_per_gas. 
+        // If so, that needs to be passed into this function or calculated differently.
+        // For now, assuming FullTransaction doesn't strictly need effective_gas_price or it's handled by the consumer.
+
+        FullTransaction {
+            to: to_address,
+            value: self.value(),
+            input: self.input().clone(),
+            nonce: self.nonce(),
+            gas_limit: self.gas_limit(),
+            gas_price: self.gas_price().map(U256::from),
+            chain_id: self.chain_id(),
             
             r: signature.r(),
             s: signature.s(),
-            v: { if signature.v() { U256::from(1) } else { U256::from(0) } }, 
-            y_parity: Some({ if signature.v() { U256::from(1) } else { U256::from(0) } }),
+            v: { // Reconstruct EIP-155 v from signature parity and chain_id
+                let y_parity_val = signature.v() as u64; // signature.v() is bool
+                if let Some(id) = self.chain_id() {
+                    U256::from(y_parity_val + 35 + id * 2)
+                } else {
+                    U256::from(y_parity_val + 27)
+                }
+            },
+            y_parity: Some(if signature.v() { U256::from(1) } else { U256::from(0) }),
             
-            access_list: envelope.access_list().map(|al_ref| {
+            access_list: self.access_list().map(|al_ref| {
                 al_ref.0.iter().map(|item| {
-                    let keys_u256 = item.storage_keys.iter().map(|k: &B256| U256::from_be_bytes(k.0)).collect();
-                    (item.address, keys_u256)
+                    (item.address, item.storage_keys.iter().map(|k| U256::from_be_bytes(k.0)).collect())
                 }).collect()
             }),
-            transaction_type: Some(U256::from(envelope.tx_type() as u8)),
-            max_fee_per_gas: Some(U256::from(envelope.max_fee_per_gas())),
-            max_priority_fee_per_gas: Some(U256::from(envelope.max_priority_fee_per_gas())),
+            
+            transaction_type: Some(U256::from(self.tx_type() as u8)),
+            
+            max_fee_per_gas: max_fee_val,
+            max_priority_fee_per_gas: self.max_priority_fee_per_gas().map(U256::from),
         }
     }
 }
@@ -78,16 +189,20 @@ impl TransactionConverter for AlloyTransaction {
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct NeuraBuiltPayloadWithDetails {
+    /// The original, underlying Ethereum built payload.
     pub original_payload: EthBuiltPayload,
+    /// A list of full transaction details corresponding to the transactions in the payload.
     pub full_transactions: Vec<FullTransaction>,
 }
 
 impl NeuraBuiltPayloadWithDetails {
+    /// Creates a new `NeuraBuiltPayloadWithDetails` instance.
     pub fn new(original_payload: EthBuiltPayload, full_transactions: Vec<FullTransaction>) -> Self {
         Self { original_payload, full_transactions }
     }
 
     // Optional: Add direct accessors if needed, e.g., for full_transactions
+    /// Returns a slice of the full transaction details.
     pub fn full_transactions(&self) -> &[FullTransaction] {
         &self.full_transactions
     }
@@ -96,14 +211,17 @@ impl NeuraBuiltPayloadWithDetails {
 impl BuiltPayload for NeuraBuiltPayloadWithDetails {
     type Primitives = EthPrimitives;
 
+    /// Returns a reference to the sealed block within the original payload.
     fn block(&self) -> &SealedBlock<<Self::Primitives as NodePrimitives>::Block> {
         self.original_payload.block()
     }
 
+    /// Returns the total fees of the original payload.
     fn fees(&self) -> U256 {
         self.original_payload.fees()
     }
 
+    /// Returns the EIP-7685 requests from the original payload, if any.
     fn requests(&self) -> Option<Requests> {
         self.original_payload.requests()
     }
